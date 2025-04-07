566bda5fc77f3c31ab630e23529e9d757f741af9
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-04-07 21:59:28 +0700

    Add navbar and theme changer

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+import { NotebookPen } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex w-full items-center mx-auto max-w-6xl justify-between h-16">
+        <div className="flex gap-4">
+          <NotebookPen />
+          <Link href="/" className="font-mono font-medium tracking-wider">
+            Ristek Quiz
+          </Link>
+        </div>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+}
