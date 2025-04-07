fc3a04729c76bc266bff5ffa8849ff26404b6de7
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-04-07 20:22:19 +0700

    Set up routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import quizRoutes from "./routes/quiz.route.js";
 
 dotenv.config();
 app = express();
 
 app.use(express.json());
+
+app.use("/api/quizzes", quizRoutes);
 
 const PORT = process.env.PORT || 5000;
 
