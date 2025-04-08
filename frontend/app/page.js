975ffe27a9239baf8da1b9a7f85c3d3ed45c7f60
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-04-08 08:00:49 +0700

    Remove particles and redundant files

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -20,25 +20,10 @@ export default function Home() {
   console.log(quizzes);
 
   return (
-    <div className="h-screen absolute top-0 w-full">
-      <div className="relative gap-4 z-50 top-0 pt-20 max-w-6xl w-full mx-auto grid grid-cols-4">
-        {quizzes.map((quiz) => (
-          <Card key={quiz._id} title={quiz.title} image={quiz.image} />
-        ))}
-      </div>
-
-      <div className="h-full w-full absolute top-0 z-0">
-        <Particles
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={200}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={false}
-          alphaParticles={true}
-          disableRotation={true}
-        />
-      </div>
+    <div className="gap-4 z-50 max-w-6xl w-full mx-auto grid grid-cols-4">
+      {quizzes.map((quiz) => (
+        <Card key={quiz._id} title={quiz.title} image={quiz.image} />
+      ))}
     </div>
   );
 }
