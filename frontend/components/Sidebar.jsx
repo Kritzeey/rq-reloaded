2b664c0ae6d69b55635132126febd2d77a43e359
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-04-08 08:49:05 +0700

    Add search and filter skeleton for sidebar

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,3 +1,16 @@
+import { Filter } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 export default function Sidebar() {
-  return <aside className="sticky top-20 mt-4 rounded-sm border-1"></aside>;
+  return (
+    <aside className="p-2 sticky top-20 mt-4 rounded-sm border-1">
+      <div className="flex gap-2">
+        <Input type="text" placeholder="Search for quizzes..." />
+        <Button size="icon" variant="outline">
+          <Filter />
+        </Button>
+      </div>
+    </aside>
+  );
 }
